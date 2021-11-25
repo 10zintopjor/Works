241d7b145cf9706f1457505d4d2cd792dd5ef276
jungtop <tenzintopjor27@gmail.com>
2021-11-25 21:14:19 +0530

    Updated Ver

--- index.py
+++ index.py
@@ -1,11 +1,20 @@
-import uuid
+from uuid import uuid4
 import csv
 import requests
 import yaml
 from convert import ewtstobo
 
+class MyDumper(yaml.Dumper):
 
-def write_mapping(bdrc_work_id, op_work_id):
+    def increase_indent(self, flow=False, indentless=False):
+        return super(MyDumper, self).increase_indent(flow, False)
+
+
+def represent_none(self, _):
+    return self.represent_scalar('tag:yaml.org,2002:null', '')
+
+
+def write_mapping(op_work_id,bdrc_work_id):
     map_file = "./works/mappings/bdrc.csv"
     with open(map_file, "a") as file:
         csvwriter = csv.writer(file)
@@ -16,6 +25,7 @@ def get_meta(bdrc_work_id, op_work_id):
     api_id = f"http://purl.bdrc.io/query/graph/OP_info?R_RES=bdr:{bdrc_work_id}&format=json"
     response = requests.get(api_id)
 
+    
     if response.status_code != 200:
         return
 
@@ -26,49 +36,71 @@ def get_meta(bdrc_work_id, op_work_id):
     meta_dict["bdrc-work-id"] = bdrc_work_id
     li = []
 
-    meta_dict["title"] = ewtstobo(content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"][
-        "http://www.w3.org/2004/02/skos/core#prefLabel"][0]["value"])
+    if isavailable("http://www.w3.org/2004/02/skos/core#prefLabel",content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"]) == True:
+        title = ewtstobo(content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"]["http://www.w3.org/2004/02/skos/core#prefLabel"][0]["value"])
+        meta_dict["title"] = title
+    else:
+        meta_dict["title"] = None
 
-    if "http://www.w3.org/2004/02/skos/core#altLabel" in content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"]:
+    if isavailable("http://www.w3.org/2004/02/skos/core#altLabel",content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"])==True:
         for id in content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"]["http://www.w3.org/2004/02/skos/core#altLabel"]:
             li.append(ewtstobo(id["value"]))
 
-    meta_dict["alternative-title"] = li
+    if not li:
+        meta_dict["alternative-title"]=None
+    else:
+        meta_dict["alternative-title"] = li
 
-    meta_dict["author"] = ""
+    meta_dict["author"] = None
 
-    meta_dict["IsRoot"] = content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"][
-        "http://purl.bdrc.io/ontology/core/isRoot"][0]["value"]
+    if isavailable("http://purl.bdrc.io/ontology/core/isRoot",content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"]) == True:
+        value = content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"]["http://purl.bdrc.io/ontology/core/isRoot"][0]["value"]
+        print(value)
+        meta_dict["isRoot"]= value.strip("'")
+    else:
+        meta_dict["isRoot"] = None
 
-    meta_dict["type-definition"] = content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"][
-        "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"][0]["value"]
+    if isavailable("http://www.w3.org/1999/02/22-rdf-syntax-ns#type",content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"]) == True:    
+        meta_dict["type-definition"] = content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"]["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"][0]["value"]
+    else:
+        meta_dict["type-definition"] = None
 
-    meta_dict["language"] = content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"][
-        "http://purl.bdrc.io/ontology/core/language"][0]["value"]
+    if isavailable("http://purl.bdrc.io/ontology/core/language",content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"]) ==True:
+        meta_dict["language"] = content[f"http://purl.bdrc.io/resource/{bdrc_work_id}"][
+            "http://purl.bdrc.io/ontology/core/language"][0]["value"]
 
-    meta_dict["wiki-data-id"] = ""
+    meta_dict["wiki-data-id"] = None
 
-    meta_dict["instances"] = ""
+    meta_dict["instances"] = None
 
     return meta_dict
 
+def isavailable(param,body):
+    if param in body:
+        return True
+    else:
+        return False    
 
 def write_works(bdrc_work_id, op_work_id):
     meta_content = get_meta(bdrc_work_id, op_work_id)
+    yaml.add_representer(type(None), represent_none)
+
     yml_file = f"./yaml/{op_work_id}.yml"
     if meta_content is None:
         return
-    with open(yml_file, "w") as file:
-        yaml.dump(meta_content, file, sort_keys=False,
-                  default_flow_style=False)
+    with open(yml_file, "w", encoding = "utf-8") as file:
+        yaml.dump(meta_content, file,Dumper=MyDumper,sort_keys=False,
+                  default_flow_style=False, allow_unicode = True)
 
+def get_uuid():
+    return uuid4().hex
 
 if __name__ == "__main__":
     file_path = "clusters-manual.csv"
     with open(file_path, "r") as csvfile:
         csvreader = csv.reader(csvfile)
         for row in csvreader:
-            op_work_id = uuid.uuid4()
-            print(op_work_id)
-            #write_mapping(row[1], op_work_id)
+            op_work_id = get_uuid()
+            write_mapping(op_work_id,row[1])
             write_works(row[1], op_work_id)
+            print("pass")
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from convert import ewtstobo
+title = "\u0F56\u0F40\u0F60\u0F0B\u0F60\u0F42\u0FB1\u0F74\u0F62\u0F0D"
 
-text = ewtstobo("bka' 'gyur/")
-print(text)
+with open("test.yml","w") as f:
+    f.write(title)
